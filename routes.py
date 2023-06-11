782d554e14e721903563ef172c4cc4117111ccdc
midesatech <midesatech@gmail.com>
2023-06-10 20:17:45 -0500

    add json control

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from flask import Flask, render_template, request, redirect
-
+from flask import Flask, render_template, request, redirect, url_for
+import json
 
 app = Flask(__name__, template_folder = "application/templates")
 
@@ -24,7 +24,9 @@ def news():
 def your_url():
     if request.method == 'POST':
         urls = {}
-        urls[request.form['code']]
+        urls[request.form['code']] = {'url':request.form['code']}
+        with open('urls.json', 'w') as url_file:
+            json.dump(urls, url_file)
         return render_template("your_url.html", code=request.form['code'])
     else:
         return redirect('/')
